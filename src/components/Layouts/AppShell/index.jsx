130940abdfcc5fc4324a62cdcc26e4cfcc87ee8c
naufalDev10 <naufalmobilelegends1@gmail.com>
2025-04-08 15:36:17 +0700

    update responseive and hero section

--- src/components/Layouts/AppShell/index.jsx
+++ src/components/Layouts/AppShell/index.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
 
-export default function AppShell({ children } = props) {
+const AppShell = ({ children } = props) => {
     return (
         <>
             <Navbar />
             <Outlet />
         </>
     )
-} 
+}
+
+export default AppShell
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -13,111 +13,88 @@ const MotionLink = motion.create(Link);
 
 const Home = () => {
     return (
-        <div className="flex justify-center items-center w-full">
-            <div className="flex flex-col gap-10 px-[7%] w-full max-w-[1440px] h-screen md:items-center md:gap-8 lg:flex-row lg:h-auto lg:pt-24 xl:h-screen xl:pt-0">
-                <div className="w-full mt-24 md:w-full md:mt-28">
-                    <motion.h2
-                        initial={{ opacity: 0, y: 35 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, ease: "easeOut" }}
-                        className="font-semibold text-center text-xl text-white md:text-center md:text-3xl lg:text-left"
-                    >
-                        Hello, <span className="text-primaryGreen">I Am</span>
-                    </motion.h2>
-                    <motion.h2
-                        initial={{ opacity: 0, y: 35 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
-                        className="inline-block w-full mt-2 font-semibold text-center text-2xl text-white md:text-center md:text-4xl md:mt-4 lg:text-left bg-red"
-                    >
-                        Naufal Ahmad Junaedi
-                    </motion.h2>
-                    <motion.h1
-                        initial={{ opacity: 0, y: 35 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 1, ease: "easeOut" }}
-                        className="inline-block w-full text-center text-4xl my-2 font-semibold text-white md:text-center md:text-5xl md:my-4 lg:text-left"
-                    >
-                        Web <span className="text-primaryGreen">Developer</span>
-                    </motion.h1>
-                    <motion.p
-                        initial={{ opacity: 0, y: 35 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
-                        className="text-white text-center md:text-center md:text-xl lg:text-left"
-                    >
-                        I am Informatics student from Digital Technology University Bandung.
-                    </motion.p>
-                    <MotionButton
-                        initial={{ opacity: 0, y: 35 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 2, ease: "easeOut" }}
-                        className="block mx-auto my-5 font-semibold bg-primaryGreen h-9 rounded-md w-[150px] cursor-pointer md:mx-auto lg:mx-0"
-                    >
-                        Download CV
-                    </MotionButton>
-                    <div className="flex justify-center items-center gap-6 mt-8 md:justify-center lg:justify-start">
-                        <MotionLink
+        <>
+            <div className="flex justify-center w-full pt-24 xl:pt-0">
+                <div className="flex flex-col justify-between items-center gap-11 w-full max-w-[1440px] md:flex-row md:px-[7%] md:gap-0 xl:h-screen">
+                    <div className="flex flex-col gap-2 px-3 xl:gap-3">
+                        <motion.h3
                             initial={{ opacity: 0, y: 30 }}
                             animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 2.2, ease: "easeOut" }}
-                            to="https://www.instagram.com/nfla"
-                            className="text-white text-2xl hover:text-primaryGreen duration-500"
-                        >
-                            <BsInstagram />
-                        </MotionLink>
-                        <MotionLink
+                            transition={{ duration: 1, ease: "easeOut" }}
+                            className="text-white text-xl text-center font-medium md:text-left lg:text-2xl xl:text-3xl">
+                            Hello, <span className="text-primaryGreen">I Am</span>
+                        </motion.h3>
+                        <motion.h2
                             initial={{ opacity: 0, y: 30 }}
                             animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 2.4, ease: "easeOut" }}
-                            to="https://github.com/naufalDev10"
-                            className="text-white text-2xl hover:text-primaryGreen duration-500"
-                        >
-                            <BsGithub />
-                        </MotionLink>
-                        <MotionLink
+                            transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
+                            className="text-white text-2xl text-center font-medium md:text-left lg:text-3xl xl:text-4xl">Naufal Ahmad Junaedi</motion.h2>
+                        <motion.h1
                             initial={{ opacity: 0, y: 30 }}
                             animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 2.6, ease: "easeOut" }}
-                            to="mailto:naufalahmadjunaedi10@gmail.com"
-                            className="text-white text-2xl hover:text-primaryGreen duration-500"
-                        >
-                            <BsEnvelopeAt />
-                        </MotionLink>
-                        <MotionLink
+                            transition={{ duration: 1, delay: 1, ease: "easeOut" }}
+                            className="text-white text-3xl text-center font-medium md:text-left lg:text-4xl xl:text-5xl">Informatics <span className="text-primaryGreen">Student</span></motion.h1>
+                        <motion.p
                             initial={{ opacity: 0, y: 30 }}
                             animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 2.8, ease: "easeOut" }}
-                            to="https://www.linkedin.com/in/naufal-ahmad-junaedi"
-                            className="text-white text-2xl hover:text-primaryGreen duration-500"
-                        >
-                            <BsLinkedin />
-                        </MotionLink>
+                            transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
+                            className="text-white text-center font-light md:text-left lg:text-md">I am Informatics student from Digital Technology University Bandung</motion.p>
+                        <MotionButton
+                            initial={{ opacity: 0, y: 30 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1, delay: 2, ease: "easeOut" }}
+                            className="bg-primaryGreen font-semibold w-32 h-9 rounded-sm mt-4 mx-auto cursor-pointer md:mx-0">
+                            Download CV
+                        </MotionButton>
+                        <div className="flex justify-center items-center gap-4 mt-4 md:justify-start md:mt-5">
+                            <MotionLink
+                                initial={{ opacity: 0, y: 30 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 1, delay: 2, ease: "easeOut" }}
+                                to="" className="text-white text-xl" >
+                                <BsInstagram />
+                            </MotionLink>
+                            <MotionLink
+                                initial={{ opacity: 0, y: 30 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 1.5, delay: 2, ease: "easeOut" }}
+                                to="" className="text-white text-xl" >
+                                <BsGithub />
+                            </MotionLink>
+                            <MotionLink
+                                initial={{ opacity: 0, y: 30 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 2, delay: 2, ease: "easeOut" }}
+                                to="" className="text-white text-xl" >
+                                <BsEnvelopeAt />
+                            </MotionLink>
+                            <MotionLink
+                                initial={{ opacity: 0, y: 30 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 2.5, delay: 2, ease: "easeOut" }}
+                                to="" className="text-white text-xl" >
+                                <BsLinkedin />
+                            </MotionLink>
+                        </div>
                     </div>
-                </div>
-                <div className="relative flex justify-center items-center lg:h-full xl:h-full">
-                    <motion.img
-                        initial={{ opacity: 0, y: 30 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 1.1, ease: "easeOut" }}
-                        src="/blob1.svg"
-                        alt="blob"
-                        className="absolute -top-16 left-[5%] translate-1.5 -z-10 md:-top-16 xl:top-12 2xl:top-44"
-                    />
-                    <div className="flex justify-center xl:h-full xl:items-end 2xl:h-auto before:content-[''] before:bg-linear-to-b before:from-transparent before:to-background">
+                    <div className="relative flex justify-center items-center">
                         <motion.img
-                            initial={{ opacity: 0, scale: 0.9 }}
+                            initial={{ opacity: 0, scale: 0 }}
                             animate={{ opacity: 1, scale: 1 }}
-                            transition={{ duration: 1, delay: 1.4, ease: "easeOut" }}
-                            src="/naufal-image.png"
-                            alt="image"
-                            className="md:w-[29.5rem] lg:w-[43rem] xl:w-[50rem"
-                        />
-                        <div className="absolute bottom-0 left-0 right-0 h-28 bg-gradient-to-t from-background to-transparent"></div>
+                            transition={{ duration: 1, delay: 1, ease: "easeOut" }}
+                            src="/blob1.svg" alt="" className="absolute -top-16 left-1/2 -translate-x-1/2 w-80 -z-10  md:-top-14 md:w-[25rem]" />
+                        <div className="relative">
+                            <motion.img
+                                initial={{ opacity: 0, scale: 0 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
+                                src="/naufal-image.png" alt="" className="w-[20rem] md:w-[25rem] lg:w-[28rem]" />
+                            <div className="absolute bottom-0 left-0 w-full h-32 bg-linear-to-b from-transparent to-background"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
